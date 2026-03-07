9dbe02606d2f7f5ef99561fce01e8abf9acdd81f
JanOPedersen <jan.o.pedersen@mail.dk>
2026-03-07 18:28:49 +0100

    use port 8000 and not 5000

--- litter-api/run.py
+++ litter-api/run.py
@@ -1,6 +1,11 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    host = os.getenv("FLASK_RUN_HOST", "127.0.0.1")
+    port = int(os.getenv("FLASK_RUN_PORT", "8000"))
+    debug = os.getenv("FLASK_DEBUG", "true").lower() == "true"
+
+    app.run(host=host, port=port, debug=debug)
