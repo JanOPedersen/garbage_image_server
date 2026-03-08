f2878cf6ccd635e28d8ea9bb5115aab2c066b425
JanOPedersen <jan.o.pedersen@mail.dk>
2026-03-08 18:02:41 +0100

    Debugging now possible

--- litter-api/run.py
+++ litter-api/run.py
@@ -6,6 +6,13 @@ app = create_app()
 if __name__ == "__main__":
     host = os.getenv("FLASK_RUN_HOST", "127.0.0.1")
     port = int(os.getenv("FLASK_RUN_PORT", "8000"))
-    debug = os.getenv("FLASK_DEBUG", "true").lower() == "true"
 
-    app.run(host=host, port=port, debug=debug)
+    #debug = os.getenv("FLASK_DEBUG", "true").lower() == "true"
+    #app.run(host=host, port=port, debug=debug)
+
+    app.run(
+        host=host,
+        port=port,
+        debug=False,
+        use_reloader=False,
+    )
